9d88a0bfe10ff73ab42f7ce366dc7d98c788f996
Nahid30 <nahidshuvo30@gmail.com>
2022-05-25 21:31:25 +0600

    use JWTTOKEN and get users data to make admin panel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -17,12 +18,31 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fl8bx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+
+}
+
+
 async function run() {
     try {
         await client.connect();
         const itemsCollection = client.db('rapid_manufacturer').collection('items');
         const purchaseCollection = client.db('rapid_manufacturer').collection('purchases');
         const reviewsCollection = client.db('rapid_manufacturer').collection('reviews');
+        const userCollection = client.db('rapid_manufacturer').collection('users');
 
         // To load all items to the client site 
         app.get('/items', async (req, res) => {
@@ -40,6 +60,50 @@ async function run() {
 
         })
 
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+
+            res.send({ result });
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        })
+
+
+
+        app.get('/item/:id', async (req, res) => {
+            const query = {};
+
+            const users = await itemsCollection.findOne(query);
+            res.send(users);
+        })
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemsCollection.findOne(query);
+            res.send(result);
+        })
 
         app.get('/item/:id', async (req, res) => {
             const id = req.params.id;
@@ -51,6 +115,7 @@ async function run() {
 
         app.get('/purchase/:email', async (req, res) => {
             const email = req.params.email;
+
             const query = { userEmail: email };
             const cursor = purchaseCollection.find(query);
             const purchase = await cursor.toArray();
@@ -58,14 +123,14 @@ async function run() {
         })
 
 
-        
+
         app.delete('/purchase/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const purchase = await purchaseCollection.deleteOne(query);
             res.send(purchase);
         })
-    
+
 
         app.post('/review', async (req, res) => {
             const review = req.body;
@@ -81,6 +146,19 @@ async function run() {
             const reviews = await cursor.toArray();
             res.send(reviews);
         })
+
+
+
+
+
+
+
+        // app.get('/purchase/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const purchase = await purchaseCollection.findOne(query);
+        //     res.send(purchase);
+        // })
 
 
     }
